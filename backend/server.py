39b909fc68496292fe1495ec6a3c1460c2334656
haashirk2003 <haashirk2003@gmail.com>
2024-09-01 15:28:08 -0400

    added switch features and error handling to most features

--- backend/server.py
+++ backend/server.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, join_room, leave_room, emit
+import random
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+hostip = "192.168.2.11"
+port = 4000
 
 # Store active rooms
 active_rooms = {}
+emptyroomkeys = []
 
 @app.route('/')
 def index():
@@ -19,37 +23,71 @@ def home():
 @socketio.on('join')
 def on_join(data):
     username = data['username']
-    room = data['room']
 
-    # If the room doesn't exist or has less than 2 users, join it
-    if room not in active_rooms:
-        active_rooms[room] = []
-    
-    if len(active_rooms[room]) < 2:
-        join_room(room)
-        active_rooms[room].append(username)
-        emit('status', {'msg': f'{username} has entered the room.'}, room=room)
+    if find_room_from_user(username) != -1:
+        emit('status', {'msg': 'You are already in a room.'})
     else:
-        emit('status', {'msg': 'Room is full.'})
+        if len(emptyroomkeys) == 0:
+            while True:
+                room = random.randint(1000, 9999)
+                if room not in active_rooms:
+                    active_rooms[room] = []
+                    break
+        else:
+            room = random.choice(emptyroomkeys)
+
+        print(active_rooms)
+        if len(active_rooms[room]) < 2:
+            join_room(room)
+            active_rooms[room].append(username)
+            emit('status', {'msg': f'{username} has entered room {room}.'}, room=room)
+        else:
+            emit('status', {'msg': 'Room is full.'})
 
 @socketio.on('message')
 def on_message(data):
-    room = data['room']
-    message = data['name'] + ': ' + data['msg']
+    username = data['name']
+    room = find_room_from_user(username)
+    message = username + ': ' + data['msg']
+    print(message + " at " + str(room))
     emit('message', {'msg': message}, room=room)
 
 @socketio.on('leave')
 def on_leave(data):
     username = data['username']
-    room = data['room']
+    room = find_room_from_user(username)
     
-    leave_room(room)
-    if room in active_rooms:
-        active_rooms[room].remove(username)
-        emit('status', {'msg': f'{username} has left the room.'}, room=room)
-        
-    if len(active_rooms[room]) == 0:
-        del active_rooms[room]
+    if room == -1:
+        emit('status', {'msg': 'You are not in a room.'})
+    else:
+        leave_room(room)
+        if room in active_rooms:
+            emit('status', {'msg': f'{username} has left the room.'}, room=room)
+            active_rooms[room].remove(username)
+            emptyroomkeys.append(room)
+            
+        if len(active_rooms[room]) == 0:
+            emptyroomkeys.remove(room)
+            del active_rooms[room]
+
+@socketio.on('disconnect')
+def on_disconnect(data):
+    on_leave(data)
+
+@socketio.on('switch')
+def on_switch(data):
+    if find_room_from_user(data['username']) != -1:
+        on_leave(data)
+        on_join(data)
+    else:
+        emit('status', {'msg': 'You are not in a room.'})
+
+def find_room_from_user(username):
+    for room in active_rooms:
+        if username in active_rooms[room]:
+            return room
+    return -1
+
 
 if __name__ == '__main__':
-    socketio.run(app, host= "127.0.0.1", port=4000, debug=True)
+    socketio.run(app, host= hostip, port=port, debug=True)
