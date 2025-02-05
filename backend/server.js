044233f23674d9d094b13f1511ec25c98da66c1d
haashirk2003 <haashirk2003@gmail.com>
2025-02-05 15:37:17 -0500

    app is dockerized

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL || "http://localhost:5000", // Frontend URL from env
+    origin: process.env.FRONTEND_URL || "http://localhost:3000", // Frontend URL from env
     methods: ["GET", "POST"],
   },
 });
